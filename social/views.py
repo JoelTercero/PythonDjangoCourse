d65c943b5e49f5a7af74b90bd0b9f66240dbba38
Joel Tercero <joel_3p@hotmail.com>
2020-12-26 02:24:10 -0600

    Creando modelos

--- social/views.py
+++ social/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 def feed(request):
-	return render(request, 'social/feed.html')
+	posts = Post.objects.all()
+
+	context = { 'posts': posts}
+	return render(request, 'social/feed.html', context)
 
 def profile(request):
 	return render(request, 'social/profile.html')
